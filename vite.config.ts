d100d27a722f1659681232ffbc2e7c19a1db9a35
aj-codecov <anthony.fazzio@sentry.io>
2024-09-03 09:33:57 -0700

    Setup Codecov Bundle Analysis

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,16 @@
-import { defineConfig } from 'vite'
-import solid from 'vite-plugin-solid'
+// vite.config.js
+import { defineConfig } from "vite";
+import { codecovVitePlugin } from "@codecov/vite-plugin";
+import { solid } from 'vite-plugin-solid'
 
 export default defineConfig({
-  plugins: [solid()],
-})
+  plugins: [
+    solid(),
+    // Put the Codecov vite plugin after all other plugins
+    codecovVitePlugin({
+      enableBundleAnalysis: process.env.CODECOV_TOKEN !== undefined,
+      bundleName: "BA-SETUP-DEMO",
+      uploadToken: process.env.CODECOV_TOKEN,
+    }),
+  ],
+});
